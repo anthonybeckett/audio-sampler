8cdbcf02b082ade1c968b17a45fd7cf0fdeb943e
Anthony Beckett <antneyb10@gmail.com>
2023-09-17 16:07:46 +0100

    Started making a sampler and ability to load and play files

---
+++ Source/PluginProcessor.h
@@ -0,0 +1,77 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/**
+*/
+class SamplerAudioProcessor  : public juce::AudioProcessor
+                            #if JucePlugin_Enable_ARA
+                             , public juce::AudioProcessorARAExtension
+                            #endif
+{
+public:
+    //==============================================================================
+    SamplerAudioProcessor();
+    ~SamplerAudioProcessor() override;
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    void loadFile();
+
+private:
+    juce::Synthesiser sampler;
+    const int numVoices{ 32 };
+
+    juce::AudioFormatManager audioFormatManager;
+    juce::AudioFormatReader* audioFormatReader{nullptr};
+
+    juce::BigInteger midiRange;
+
+    const int midiNoteC3 = 60;
+    double attackTime = 0.1;
+    double releaseTime = 0.1;
+    double maxTimeInSeconds = 10.0;
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SamplerAudioProcessor)
+};
